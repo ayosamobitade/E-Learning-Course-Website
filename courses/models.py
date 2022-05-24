41f50a78c05d811836179541d09a12f33a2ba2a8
UncleAyo <ayosamobitade@gmail.com>
2022-05-24 18:25:32 +0100

    added model

--- courses/models.py
+++ courses/models.py
@@ -28,4 +28,12 @@ class Course(models.Model):
         return self.title
 
 class Module(models.Model):
-    course = models.ForeignKey(Course, )
+    course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
+    title = models.CharField(max_length =200)
+    description = models.TextField(blank = True)
+
+
+    def __str__(self):
+        return self.title
+
+
--- eduversity/settings.py
+++ eduversity/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses.apps.CourseConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
