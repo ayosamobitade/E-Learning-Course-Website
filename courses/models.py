ef15e2c291b46202fff5eb4ae4b9737be918c2b0
UncleAyo <ayosamobitade@gmail.com>
2022-08-22 22:52:27 +0100

    some update

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Subject(models.Model):
     title = models.CharField(max_length = 200)
@@ -37,3 +39,6 @@ class Module(models.Model):
         return self.title
 
 
+class Content(models.Model):
+    module = models.ForeignKey(module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
