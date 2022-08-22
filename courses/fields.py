5f62dc95b96daa418adfbbc8dbef3a2ce9ccdc60
UncleAyo <ayosamobitade@gmail.com>
2022-08-22 23:38:23 +0100

    some update

--- courses/fields.py
+++ courses/fields.py
@@ -1,3 +1,6 @@
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 
+class OrderField(models.PositiveBigIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        self.for_fields = for_fields
