808b6364eb9a03625d54102460510a0dc67968e8
UncleAyo <ayosamobitade@gmail.com>
2023-03-16 08:31:13 +0100

    some updates

--- courses/models.py
+++ courses/models.py
@@ -38,11 +38,13 @@ class Module(models.Model):
     description = models.TextField(blank = True)
     order = OrderField(blank=True, for_fields = ['course'])
 
+    class Meta:
+        ordering = ['order']
+
     def __str__(self):
         return f'{self.order}. {self.title}'
     
-    class Meta:
-        ordering = ['order']
+
 
 
 class Content(models.Model):
