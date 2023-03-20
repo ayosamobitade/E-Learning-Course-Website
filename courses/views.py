377c383da8bcdc2162232d92b77ddc6b74fcd597
UncleAyo <ayosamobitade@gmail.com>
2023-03-20 08:26:50 +0100

    some updates

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from .models import Course
+
+class ManageCourseListView(ListView)
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
