63cc67c08a5146e592db11d8596b416e91427dc5
UncleAyo <ayosamobitade@gmail.com>
2023-03-20 08:45:34 +0100

    some updates

--- courses/views.py
+++ courses/views.py
@@ -3,11 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.list import ListView
 from .models import Course
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-class ManageCourseListView(ListView)
+class ManageCourseListView(ListView):
     model = Course
     template_name = 'courses/manage/course/list.html'
 
     def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
+
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/list.html'
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
