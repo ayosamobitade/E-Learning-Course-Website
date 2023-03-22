a1fc1e59bc15fe173eb5c8e6b1f0d0ee43577361
UncleAyo <ayosamobitade@gmail.com>
2023-03-22 09:54:03 +0100

    some updates

--- courses/views.py
+++ courses/views.py
@@ -55,4 +55,25 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
     permission_required = 'courses.delete_course'
 
 
-# 
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, 
+                                        id = pk,
+                                        owner= request.user)
+        return super().dispatch(request, pk)
+    
+    def get(self, requet, *arg, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+    
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course':self.course,
+                                        'formset':formset})
