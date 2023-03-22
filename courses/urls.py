b517f1554c2fd44eec7310e10d6c1ee8eb2905da
UncleAyo <ayosamobitade@gmail.com>
2023-03-22 10:12:01 +0100

    some updates

--- courses/urls.py
+++ courses/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('mine/', views.ManageCourseListView.as_view(), name = 'manage_course_list'),
-    path('create/', views.CourseCreateView.as_view(), name = 'course_create'),
+    path('create/', views.CourseCreateView.as_view(), name = 'course_created'),
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name = 'course_edit'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name = 'course_delete'),
 ]
