faba475ddb57e50212faf4298440c5bf0cea3ffd
UncleAyo <ayosamobitade@gmail.com>
2023-03-23 09:23:18 +0100

    some updates

--- courses/urls.py
+++ courses/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(), name = 'module_content_create'),
     path('module/<int:module_id>/content/<model_name>/<id>/', views.ContentCreateUpdateView.as_view(), name = 'model_content_update'),
     path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name = 'module_content_delete'),
+    path('module/<int:module_id>/', views.ModuleContentListView.as_view(), name='module_content_list'),
 ]
