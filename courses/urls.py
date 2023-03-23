133bd1b9530b8d1256561482d6efef966ca0288b
UncleAyo <ayosamobitade@gmail.com>
2023-03-23 08:59:40 +0100

    some updates

--- courses/urls.py
+++ courses/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
     path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(), name = 'module_content_create'),
     path('module/<int:module_id>/content/<model_name>/<id>/', views.ContentCreateUpdateView.as_view(), name = 'model_content_update'),
+    path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name = 'module_content_delete'),
 ]
