f9b0547938a2d6fbbbaf03369a94070b0dc2b7e3
UncleAyo <ayosamobitade@gmail.com>
2023-03-27 11:30:11 +0100

    some updates

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.StudentRegistrationView.as_view(), name = 'student_registration')
+    path('register/', views.StudentRegistrationView.as_view(), name = 'student_registration'),
 ]
