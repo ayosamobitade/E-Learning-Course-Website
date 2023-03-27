9e9423d8c38db52b42ac0e9fa31c434c10de9bf9
UncleAyo <ayosamobitade@gmail.com>
2023-03-27 11:38:28 +0100

    some updates

--- students/views.py
+++ students/views.py
@@ -26,4 +26,15 @@ class StudentRegistrationView(CreateView):
         return result
     
     # enrolling on courses
+class StudentEnrollingCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
 
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args = [self.course.id])
+    
