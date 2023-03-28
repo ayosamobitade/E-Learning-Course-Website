2641ef6aa8776c73b4bbb6c410794a92d6158a5e
UncleAyo <ayosamobitade@gmail.com>
2023-03-28 11:13:13 +0100

    some updates

--- courses/models.py
+++ courses/models.py
@@ -6,6 +6,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from .fields import OrderField
 
+# rending different types of content
+from django.template.loader import render_to_string
 
 class Subject(models.Model):
     title = models.CharField(max_length = 200)
@@ -69,6 +71,12 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item':self}
+        )
 
 
 class Text(ItemBase):
