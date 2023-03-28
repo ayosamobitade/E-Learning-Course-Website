29b953420f6ffff80e5142ce39b0a2df2ab4278a
UncleAyo <ayosamobitade@gmail.com>
2023-03-28 10:54:58 +0100

    some updates

--- eduversity/settings.py
+++ eduversity/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#redirecting the studnets to a url
+from django.urls import reverse_lazy
+LOGIN_REDIRECTING_URL = reverse_lazy('student_course_list')
