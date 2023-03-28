8c0c9fe8abe9dadb85376979886be3111e267b3e
UncleAyo <ayosamobitade@gmail.com>
2023-03-28 11:27:23 +0100

    some updates

--- eduversity/urls.py
+++ eduversity/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name = 'login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name = 'logout'),
@@ -27,4 +30,6 @@ urlpatterns = [
     path('students/', include('students.urls')),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root = settings.MEDIA_ROOT)
