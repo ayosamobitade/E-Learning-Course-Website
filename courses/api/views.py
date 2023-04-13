3cb5442d8be48e8adc913155a3c591d0625e4d9f
UncleAyo <ayosamobitade@gmail.com>
2023-04-13 19:11:08 +0100

    some updates

--- courses/api/views.py
+++ courses/api/views.py
@@ -11,4 +11,19 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
+
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models import Course
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.User)
+        return Response({'enrolled': True})
+
+
+
     
