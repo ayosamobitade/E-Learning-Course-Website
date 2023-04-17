7757878b3f6b6dbeaa13a6423e3095f4e5f1bb5e
UncleAyo <ayosamobitade@gmail.com>
2023-04-17 09:34:15 +0100

    some updates

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -10,6 +10,6 @@ app_name = 'courses'
 urlpatterns = [
     path('subjects/', views.SubjectListView.as_view(), name = 'subject_list'),
     path('subjects/<pk>/', views.SubjectDetailView.as_view(), name = 'subject_detail'),
-    path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name = 'course_enroll'),
+    #path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name = 'course_enroll'),
     path('', include(router.urls)),
 ]
