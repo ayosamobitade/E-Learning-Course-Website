a185f85162223a54efec050746c01a095ce4f901
UncleAyo <ayosamobitade@gmail.com>
2023-04-17 12:13:50 +0100

    some updates

--- eduversity/routing.py
+++ eduversity/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 application = ProtocolTypeRouter({
-    # empty for now
+    'websocket': AuthMiddlewareStack(
+    URLRouter(
+    chat.routing.websocket_urlpatterns
+    )
+
+    )
 })
