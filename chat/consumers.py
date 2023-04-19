389db2111a37e5d7c0f0aa32157911aa409fd841
UncleAyo <ayosamobitade@gmail.com>
2023-04-19 08:46:40 +0100

    some updates

--- chat/consumers.py
+++ chat/consumers.py
@@ -25,5 +25,13 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+            'type': 'chat_message',
+            'message': message,
+            }
+        )
         # send message to Websocket
-        self.send(text_data = json.dump({'message':message}))
+        #self.send(text_data = json.dump({'message':message}))
