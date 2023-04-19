1e1a0d0874f7398b07051c838db4b4a0a3a1b826
UncleAyo <ayosamobitade@gmail.com>
2023-04-19 08:40:02 +0100

    some updates

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,16 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = 'chat_%s' %self.id
+        #join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         # accept connections
         self.accept()
 
--- eduversity/settings.py
+++ eduversity/settings.py
@@ -162,7 +162,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'host': [('127.0.0.1', 6379)],
+            'hosts': [('127.0.0.1', 6379)],
         },
     },
 }
