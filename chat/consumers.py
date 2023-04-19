2161fa9995230e0f5c8617fde6064e4f22fdc2e3
UncleAyo <ayosamobitade@gmail.com>
2023-04-19 08:42:51 +0100

    some updates

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,7 +15,11 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # leave the room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # receive message from WebSocket
     def receive(self, text_data):
