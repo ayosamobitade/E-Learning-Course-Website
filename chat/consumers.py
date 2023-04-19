8beb6e0abc0c5c706a0c806333540e777e90b31e
UncleAyo <ayosamobitade@gmail.com>
2023-04-19 08:54:58 +0100

    some updates

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,11 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = 'chat_%s' % self.id
         #join room group
@@ -25,12 +27,15 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
         # send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
             'type': 'chat_message',
             'message': message,
+            'user': self.user.username,
+            'datetime': now.isoformat(),
             }
         )
     
