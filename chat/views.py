ea73835a506156a62d41096c88d5bce4385dfabe
UncleAyo <ayosamobitade@gmail.com>
2023-04-30 17:32:26 +0100

    some updates

--- chat/views.py
+++ chat/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def course_chat_room(request, course_id):
     try:
-        #retrieve course with given id joined by the current use
+        #retrieve course with given id joined by the current user
         course = request.user.course_joined.get(id = course_id)
     except:
         #user is not a student of the course or course does not exist
